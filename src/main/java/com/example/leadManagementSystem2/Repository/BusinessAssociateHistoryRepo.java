bc92b2fa35575d718d2a518f36404a56ca93a96c
shriv <shriv@LAPTOP-PSGBDAEE>
2024-03-01 01:20:50 +0530

    fixed baHistory repo and user repo

--- src/main/java/com/example/leadManagementSystem2/Repository/BusinessAssociateHistoryRepo.java
+++ src/main/java/com/example/leadManagementSystem2/Repository/BusinessAssociateHistoryRepo.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.leadManagementSystem2.Entity.BusinessAssociateHistory;
 
-public interface BusinessAssociateHistoryRepo extends JpaRepository<BusinessAssociateHistory, Integer>{
+public interface BusinessAssociateHistoryRepo extends JpaRepository<BusinessAssociateHistory, Long>{
 
 }
