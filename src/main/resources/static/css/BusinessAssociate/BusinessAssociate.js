e7cae64daba6de94d693cd6cf1d155f123b5836e
aditi-0312 <shrivastvaaditi@gmail.com>
2024-04-11 13:37:55 +0530

    alert on disabled checkbox

--- src/main/resources/static/css/BusinessAssociate/BusinessAssociate.js
+++ src/main/resources/static/css/BusinessAssociate/BusinessAssociate.js
@@ -14,61 +14,61 @@ function copyLink() {
 }
 
 function updateQRCodeLink() {
-		var bsurl = $('#copyLinkInput').val();
-		console.log(bsurl);
-		
-		
-		var baId = $('#businessAssociateId').val();
-		console.log(baId);
-		
-		var url = "http://localhost:8080/businessAssociate/businessAssociateDashboard/QRCodePage?partyid=" + baId;
-		console.log(url);
+    var bsurl = $('#copyLinkInput').val();
+    console.log(bsurl);
 
-        window.location=url;
-    }
-    
+
+    var baId = $('#businessAssociateId').val();
+    console.log(baId);
+
+    var url = "http://localhost:8080/businessAssociate/businessAssociateDashboard/QRCodePage?partyid=" + baId;
+    console.log(url);
+
+    window.location = url;
+}
+
 function transferRequest() {
     const transferButton = document.getElementById('transferAmount');
     const amount = document.querySelectorAll('.amount-checkbox');
 
     const selectedAmount = [];
-    var key=0;
+    var key = 0;
     amount.forEach(checkbox => {
         if (checkbox.checked) {
-            selectedAmount[key]=checkbox.value;
-            key+=1;
+            selectedAmount[key] = checkbox.value;
+            key += 1;
             //selectedAmount.push(checkbox.value);
         }
     });
 
-    var amountToTransfer={amount:selectedAmount};
+    var amountToTransfer = { amount: selectedAmount };
 
     console.log(selectedAmount);
 
-    const data = {amount: selectedAmount};
+    const data = { amount: selectedAmount };
 
-fetch('http://localhost:8080/businessAssociate/businessAssociateDashboard/WalletRequest', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data),
-})
-.then(response => {
-    if (!response.ok) {
-        throw new Error('Network response was not ok');
-    }
-    return response.json();
-})
-.then(data => {
-    console.log('Response from server:', data);
-     // Update the UI to display the total amount
-     const totalAmountSpan = document.getElementById('totalAmount');
-     totalAmountSpan.textContent = data.totalAmount;
-})
-.catch(error => {
-    console.error('There was a problem with your fetch operation:', error);
-});
+    fetch('http://localhost:8080/businessAssociate/businessAssociateDashboard/WalletRequest', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log('Response from server:', data);
+            // Update the UI to display the total amount
+            const totalAmountSpan = document.getElementById('totalAmount');
+            totalAmountSpan.textContent = data.totalAmount;
+        })
+        .catch(error => {
+            console.error('There was a problem with your fetch operation:', error);
+        });
 
 }
 
@@ -79,43 +79,43 @@ function totalAmountTransfer() {
     const amount = document.querySelectorAll('.amount-checkbox');
 
     const selectedAmount = [];
-    var key=0;
+    var key = 0;
     amount.forEach(checkbox => {
         if (checkbox.checked) {
-            selectedAmount[key]=checkbox.value;
-            key+=1;
+            selectedAmount[key] = checkbox.value;
+            key += 1;
             //selectedAmount.push(checkbox.value);
         }
     });
 
-    var amountToTransfer={amount:selectedAmount};
+    var amountToTransfer = { amount: selectedAmount };
 
     console.log(selectedAmount);
 
-    const data = {amount: selectedAmount};
+    const data = { amount: selectedAmount };
 
-fetch('http://localhost:8080/businessAssociate/businessAssociateDashboard/totalAmount', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data),
-})
-.then(response => {
-    if (!response.ok) {
-        throw new Error('Network response was not ok');
-    }
-    return response.json();
-})
-.then(data => {
-    console.log('Response from server:', data);
-     // Update the UI to display the total amount
-     const totalAmountSpan = document.getElementById('totalAmount');
-     totalAmountSpan.textContent = data.totalAmount;
-})
-.catch(error => {
-    console.error('There was a problem with your fetch operation:', error);
-});
+    fetch('http://localhost:8080/businessAssociate/businessAssociateDashboard/totalAmount', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log('Response from server:', data);
+            // Update the UI to display the total amount
+            const totalAmountSpan = document.getElementById('totalAmount');
+            totalAmountSpan.textContent = data.totalAmount;
+        })
+        .catch(error => {
+            console.error('There was a problem with your fetch operation:', error);
+        });
 
 }
 
@@ -169,6 +169,20 @@ document.addEventListener("DOMContentLoaded", function () {
             const checkbox = row.querySelector('.amount-checkbox');
             checkbox.disabled = false;
         }
+        else {
+            const checkboxes = row.querySelectorAll('.checkbox');
+            checkboxes.forEach((checkbox) => {
+                checkbox.addEventListener('click', function () {
+
+                    var inp = document.getElementById('myCheckbox');
+                if (inp.disabled) {
+                    //console.log('disabled input clicked!');
+                    alert("It has not been 30 days");
+                }
+                });
+            });
+        }
+
     });
 });
 
